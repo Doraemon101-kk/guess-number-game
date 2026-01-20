91cbfa87e62307dec9bc32defb3d2b384f24759c
Doraemon101-kk <2373459390@qq.com>
2026-01-21 00:28:41 +0800

    refactor(guess): extract helper functions, keep only play_single_round

--- guess_number/guess.py
+++ guess_number/guess.py
@@ -1,7 +1,7 @@
 from random import randint
-from sys import argv
+from .utils import *
 
-def guess(min_num, max_num):
+def guess_single_round(min_num, max_num):
 
     num = randint(min_num, max_num)
     count = 0
@@ -13,51 +13,17 @@ def guess(min_num, max_num):
 
         count += 1
 
-        if not number_in_range(min_num, guess, max_num):
+        if not is_in_range(min_num, guess, max_num):
             print("Out of range")
             continue
 
         if guess == num:
             print("Bingo!")
             print(f"You played {count} times to guess it.")
-            break
+            return count
+        
         elif guess < num:
             print("Too small")
             
         else:
             print("Too big")
-
-def number_in_range(min_num, num, max_num):
-    return min_num <= num <= max_num
-
-def get_valid_input():
-    while True:     
-        s = input().strip()
-
-        if not s:
-            print("Please give a integer number")
-            continue
-
-        try:
-            return int(s)
-        except ValueError:
-            print("Please give a integer number")
-            continue
-
-def parse_args():
-    default_min, default_max = 1, 50
-    
-    if len(argv) == 3:
-        try:
-            a = int(argv[1])
-            b = int(argv[2]) 
-            if a < b:
-                return a, b
-        except ValueError:
-            pass
-        
-    return default_min, default_max
-
-if __name__ == "__main__":
-    min_num, max_num = parse_args()
-    guess(min_num, max_num)
